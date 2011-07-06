4b8b75f72e25df0741aff30a9c26fa545ebf7679
arthurthiago <arthurthiago@b0546e4c-2a04-ba31-302b-a0dd271981f2>
2011-07-06 23:40:51 +0000

    * Enviando alterações que o Daniel fez

--- trunk/jvm/Debug/Main.java
+++ trunk/jvm/Debug/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		Humano julio = new Humano();
 		julio.setNome("Julio");
 		julio.andar(10);
+		float dois = 2;
+		float tres = 3;
+		double zero = 0;
+		System.out.println(zero*dois*tres);
 	}
 
 }
